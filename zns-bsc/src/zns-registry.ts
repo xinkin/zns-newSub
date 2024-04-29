19c137c56d726f467b1cc39f5a076a08245e2f6d
Yash <91452674+xinkin@users.noreply.github.com>
2024-04-29 14:32:47 +0530

    new sub

--- zns-bsc/src/zns-registry.ts
+++ zns-bsc/src/zns-registry.ts
@@ -1,219 +1,87 @@
-import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  BatchMetadataUpdate as BatchMetadataUpdateEvent,
-  MetadataUpdate as MetadataUpdateEvent,
-  MintedDomain as MintedDomainEvent,
-  Paused as PausedEvent,
-  PrimaryDomainSet as PrimaryDomainSetEvent,
-  RenewedDomain as RenewedDomainEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
-  Transfer as TransferEvent,
-  Unpaused as UnpausedEvent
-} from "../generated/ZNSRegistry/ZNSRegistry"
-import {
-  Approval,
-  ApprovalForAll,
-  BatchMetadataUpdate,
-  MetadataUpdate,
-  MintedDomain,
-  Paused,
-  PrimaryDomainSet,
-  RenewedDomain,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer,
-  Unpaused
-} from "../generated/schema"
+import { MintedDomain as MintedDomainEvent, PrimaryDomainSet as PrimaryDomainSetEvent, RenewedDomain as RenewedDomainEvent, Transfer as TransferEvent } from "../generated/ZNSRegistry/ZNSRegistry"
+import { Domain, Account, Transfer, Registration, ExpiryExtended } from "../generated/schema"
+import { crypto, Bytes } from '@graphprotocol/graph-ts'
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBatchMetadataUpdate(
-  event: BatchMetadataUpdateEvent
-): void {
-  let entity = new BatchMetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._fromTokenId = event.params._fromTokenId
-  entity._toTokenId = event.params._toTokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
-  let entity = new MetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleMintedDomain(event: MintedDomainEvent): void {
-  let entity = new MintedDomain(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.domainName = event.params.domainName
-  entity.tokenId = event.params.tokenId
-  entity.owner = event.params.owner
-  entity.expiry = event.params.expiry
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let domain = new Domain(event.params.tokenId.toString())
+  domain.name = event.params.domainName
+  domain.createdAt = event.block.timestamp
+  domain.expiryDate = event.params.expiry
+  domain.labelName = event.params.domainName
+  domain.labelhash = Bytes.fromByteArray(crypto.keccak256(Bytes.fromUTF8(domain.labelName!)))
+  domain.resolvedAddress = event.params.owner.toHex()
+  domain.registrant = event.params.owner.toHex()
+  let ownerId = event.params.owner.toHex()
+  let owner = Account.load(ownerId)
+  if (!owner) {
+    owner = new Account(ownerId)
+  }
+  owner.save()
+  domain.owner = owner.id
+  domain.save()
+  let mintedEvent = new Registration(event.params.tokenId.toString())
+  mintedEvent.registrant = event.params.owner.toHex()
+  mintedEvent.domain = domain.id
+  mintedEvent.registrationDate = event.block.timestamp
+  mintedEvent.expiryDate = event.params.expiry
+  mintedEvent.cost = event.transaction.value
+  mintedEvent.save()
 }
 
 export function handlePrimaryDomainSet(event: PrimaryDomainSetEvent): void {
-  let entity = new PrimaryDomainSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.owner = event.params.owner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let domain = Domain.load(event.params.tokenId.toString())
+  if (!domain) {
+    return
+  }
+  let ownerId = event.params.owner.toHex()
+  let owner = Account.load(ownerId)
+  if (!owner) {
+    owner = new Account(ownerId)
+  }
+  domain.owner = owner.id
+  domain.save()
+  owner.save()
 }
 
 export function handleRenewedDomain(event: RenewedDomainEvent): void {
-  let entity = new RenewedDomain(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.expiry = event.params.expiry
+  let domain = Domain.load(event.params.tokenId.toString())
+  if (!domain) {
+    return
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  domain.expiryDate = event.params.expiry
+  domain.save()
 
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+   let expiryExtendedId = event.transaction.hash.toHexString().concat('-').concat(event.logIndex.toString())
+  let expiryExtended = ExpiryExtended.load(expiryExtendedId)
+  if (!expiryExtended) {
+    expiryExtended = new ExpiryExtended(expiryExtendedId)
+    expiryExtended.domain = domain.id
+    expiryExtended.blockNumber = event.block.number.toI32()
+    expiryExtended.transactionID = event.transaction.hash
+    expiryExtended.expiryDate = event.params.expiry
+    expiryExtended.save()
+  }
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let domain = Domain.load(event.params.tokenId.toHex())
+  if (!domain) {
+    return
+  }
+  let transfer = new Transfer(event.transaction.hash.toHex())
+  transfer.domain = domain.id
+  transfer.owner = event.params.to.toHex()
+  transfer.blockNumber = event.block.number.toI32()
+  transfer.transactionID = event.transaction.hash
+  let toOwnerId = event.params.to.toHex()
+  let toOwner = Account.load(toOwnerId)
+  if (!toOwner) {
+    toOwner = new Account(toOwnerId)
+  }
+  domain.owner = toOwner.id
+  transfer.save()
+  domain.save()
+  toOwner.save()
 }
